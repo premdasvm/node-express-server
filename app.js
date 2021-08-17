7769ed74bfd76ca1e9f9ffb22cbcfe730d4c42af
Prem <premdasvm8@gmail.com>
2021-08-17 12:26:04 +0530

    [New]

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 require("dotenv").config();
+const https = require("https");
+const fs = require("fs");
+
+const privateKey = fs.readFileSync("ssl/easternapps_dyndns_org.key", "utf8");
+const certificate = fs.readFileSync("ssl/easternapps_dyndns_org.crt", "utf8");
+const ca = fs.readFileSync("ssl/easternapps_dyndns_org.ca-bundle", "utf8");
+
+const credentials = { key: privateKey, cert: certificate, ca: ca };
+
 const app = express();
 const port = process.env.PORT;
-
-app.listen(port, () => {
-  console.log(`Express Server is running at http://localhost:${port}`);
-});
-
 app.use("/", express.static("dist"));
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port);
